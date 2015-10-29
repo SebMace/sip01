46e9092a0e8093905a09562b7af62223567497c8
smace <sebmace78@gmail.com>
2015-10-29 17:23:50 +0100

    creation de findAll JdbcAccountDao pour utiliser l'appli correction.

--- src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
+++ src/main/java/com/springinpractice/ch01/dao/jdbc/JdbcAccountDao.java
@@ -7,10 +7,19 @@
  */
 package com.springinpractice.ch01.dao.jdbc;
 
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.sql.DataSource;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.ConnectionImpl;
 import com.springinpractice.ch01.dao.AccountDao;
 import com.springinpractice.ch01.model.Account;
 
@@ -25,7 +34,21 @@ public class JdbcAccountDao implements AccountDao {
 	}
 
 	public List<Account> findAll() throws Exception {
-		throw new UnsupportedOperationException("This method has not been implemented");
-	}	
-	
+		List<Account> results = new ArrayList<Account>();
+		 java.sql.Connection conn = dataSource.getConnection();
+		 String sqlStmt = "Select * from account2";
+		 Statement stmt = conn.prepareStatement(sqlStmt);
+		 ResultSet rs = stmt.executeQuery(sqlStmt);
+		 DateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");	
+		 while (rs.next())
+		 {			 
+			 String firstColumn = rs.getString(1);
+			 String secondColumn = rs.getString(2);
+			 String thirdColumn = rs.getString(3);
+			 System.out.println("First : " + firstColumn + ", second : " + secondColumn + ", third : " + thirdColumn ); 
+			 Account cpte= new Account(firstColumn,new BigDecimal(secondColumn), fmt.parse(thirdColumn));
+			 results.add(cpte);
+		 }		
+		return results;
+	}		
 }
